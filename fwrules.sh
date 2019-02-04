0195e7625bf88900315ecf4f2fa8d009f951ebee
GrigorDimitrov <b00099288@student.itb.ie>
2019-02-04 09:51:07 -0800

    04 February

--- fwrules.sh
+++ fwrules.sh
@@ -20,24 +20,24 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 #Allow HTTP and HTTPS traffic
-iptables -A INPUT -i ens33 -p tcp -m tcp --dport 80 -d $SERVER_IP -j ACCEPT
-iptables -A INPUT -i ens33 -p tcp -m tcp --dport 443 -d $SERVER_IP -j ACCEPT
+iptables -A INPUT -i enp0s3 -p tcp -m tcp --dport 80 -d $SERVER_IP -j ACCEPT
+iptables -A INPUT -i enp0s3 -p tcp -m tcp --dport 443 -d $SERVER_IP -j ACCEPT
 
 #Allow SSH and FTP access for ADMIN_IP
-iptables -A INPUT -i ens33 -p tcp -m tcp --dport 21 -s $ADMIN_IP -d $SERVER_IP -j ACCEPT
-iptables -A INPUT -i ens33 -p tcp -m tcp --dport 22 -s $ADMIN_IP -d $SERVER_IP -j ACCEPT
+iptables -A INPUT -i enp0s3 -p tcp -m tcp --dport 21 -s $ADMIN_IP -d $SERVER_IP -j ACCEPT
+iptables -A INPUT -i enp0s3 -p tcp -m tcp --dport 22 -s $ADMIN_IP -d $SERVER_IP -j ACCEPT
 
-#Log pinging abuse
+#Logging abuse
 iptables -A INPUT -p icmp -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix PING-DROP:
 
-#Allow icmp echo requests
-#iptables -A INPUT -i ens33 -p icmp --icmp-type 8 -d $SERVER_IP -j ACCEPT
+#Allow admin icmp echo requests
+iptables -A INPUT -i enp0s3 -p icmp -s $ADMIN_IP --icmp-type 8 -j ACCEPT
 
-#Limiting the incoming icmp ping request:
-iptables -A INPUT -p icmp --icmp-type echo-request -m limit --limit 1/s --limit-burst 5 -j ACCEPT
+#Disallow icmp echo requests
+iptables -A INPUT -i enp0s3 -p icmp --icmp-type 8 -j DROP
 
-#Prevent SYN flood attack
-iptables -A INPUT -p tcp --syn -m limit --limit 1/s --limit-burst 3 -j ACCEPT
+#Limiting the incoming icmp request:
+iptables -A INPUT -p icmp -m limit --limit  1/s --limit-burst 1 -j ACCEPT
 
 ### DROP spoofing packets
 iptables -A INPUT -s 10.0.0.0/8 -j DROP 
@@ -59,6 +59,7 @@ iptables -A INPUT -d 255.255.255.255 -j DROP
 iptables -A INPUT -s $SERVER_IP -j DROP
 
 # Droping all invalid packets
+iptables -A INPUT -m state --state INVALID -j LOG --log-prefix "Invalid Packet Found"
 iptables -A INPUT -m state --state INVALID -j DROP
 iptables -A FORWARD -m state --state INVALID -j DROP
 iptables -A OUTPUT -m state --state INVALID -j DROP
@@ -73,11 +74,21 @@ iptables -A INPUT -p tcp -m recent --name portscan --set -j DROP
 iptables -A FORWARD -p tcp -m recent --name portscan --set -m limit -j LOG --log-prefix "portscan:"
 iptables -A FORWARD -p tcp -m recent --name portscan --set -j DROP
 
+#Prevent SYN flood attack
+#iptables -A INPUT -p tcp --syn -m limit --limit 1/s --limit-burst 1 -j LOG --log-prefix SYN-ABUSE-DROPPED:
+#iptables -A INPUT -p tcp --syn -m limit --limit 1/s --limit-burst 1 -j ACCEPT
+
+iptables -N DROPSYN
+iptables -A DROPSYN -m limit -j LOG --log-prefix "syn attack:"
+iptables -A DROPSYN -j DROP
+iptables -I INPUT -p tcp --syn -i enp0s3 -m state --state NEW -m recent --set
+iptables -I INPUT -p tcp --syn -i enp0s3 -m state --state NEW -m recent  --update --seconds 2 --hitcount 4 -j DROPSYN
+
 
 #Prevent SSH brute-force attacks
 iptables -N LOGDROP
 iptables -A LOGDROP -j LOG --log-prefix "ssh attack:"
 iptables -A LOGDROP -j DROP
-iptables -I INPUT -p tcp --dport 22 -i ens33 -m state --state NEW -m recent --set
-iptables -I INPUT -p tcp --dport 22 -i ens33 -m state --state NEW -m recent  --update --seconds 60 --hitcount 4 -j LOGDROP
+iptables -I INPUT -p tcp --dport 22 -i enp0s3 -m state --state NEW -m recent --set
+iptables -I INPUT -p tcp --dport 22 -i enp0s3 -m state --state NEW -m recent  --update --seconds 60 --hitcount 4 -j LOGDROP
 
